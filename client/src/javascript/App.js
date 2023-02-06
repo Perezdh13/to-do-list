5fb6cba5c8e3642f94a405bd823d782b3ba03212
perezdh13@gmail.com <Perezdh13@gmail.com>
2023-02-06 14:04:34 +0100

    first commit and server

---
+++ client/src/javascript/App.js
@@ -0,0 +1,12 @@
+import MainPage from './ServerPage';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/javascript/ServerPage.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+import Axios from 'axios'
+
+function MainPage() {
+
+    const [taskName, setTaskName] = useState('')
+    const [taskType, setTaskType] = useState('')
+    const [taskDay, setTaskDay] = useState('')
+    const [taskHour, setTaskHour] = useState('')
+    const [taskDescription, setTaskDescription] = useState('')
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [type, setType] = useState('');
+    const [day, setDay] = useState('');
+    const [hour, setHour] = useState('');
+
+    const submit = () => {
+        Axios.post("http://localhost:3001/", {
+            taskName: name,
+            taskDescription: description,
+            taskType: type,
+            taskDay: day,
+            taskHour: hour,
+        }).then(() => {
+            alert('tarea añadida');
+        })
+
+    };
+
+    return (
+        <div>
+            <h1> TO-DO LIST</h1>
+            <div className='main-page-enterData'>
+                <label>task Name</label>
+                <input type='text' name='name' onChange={(e) => { setTaskName(e.target.value) }} />
+                <label>task Type</label>
+                <input type='text' name='description' onChange={(e) => { setTaskType(e.target.value) }} />
+                <label>Task Day</label>
+                <input type='text' name="type" onChange={(e) => { setTaskDay(e.target.value) }} />
+                <label>Task Hour</label>
+                <input type='text' name="day" onChange={(e) => { setTaskHour(e.target.value) }} />
+                <label>task Description</label>
+                <input type='text' name="hour" onChange={(e) => { setTaskDescription(e.target.value) }} />
+                <button onClick={submit} >Submit</button>
+            </div>
+        </div>
+    )
+}
+
+export default MainPage
